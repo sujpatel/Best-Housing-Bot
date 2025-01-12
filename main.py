07ef8b2ac138b5cfc4bd03003f749045140e9a4d
sujpatel <sujaypat3@gmail.com>
2025-01-12 11:57:58 -0500

    added more features, working on getting the listings

--- main.py
+++ main.py
@@ -5,39 +5,76 @@ from datetime import datetime #included by default, used for working with dates 
 import asyncio #included by default , allows bot to perform tasks concurrently 
 
 
-DISCORD_TOKEN = "your-discord-token" #bot's token needed to authenticate with Discord's API
-CHANNEL_ID = 123456789 #ID of DIscord channel where bot will send updates
-RAPIDAPI_KEY = "your-rapidapi-key" #RapidAPI key for authenticating API requests
-CITY = "New Brunswick" #target city for rental searches
-STATE = "NJ" #state abbreviation for rental search
-MAX_PRICE = 2000 #sets maximum rent value for listings 
+DISCORD_TOKEN = "your-discord-token"
+CHANNEL_ID = 123456789 
+RAPIDAPI_KEY = "your-rapidapi-key"
+CITY = "New Brunswick" 
+STATE = "NJ" 
+MAX_PRICE = 2000 
 
 class SimpleHousingBot(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
-        self.create_database()
+        self.setup_database()
         
-    def create_database(self):
-        db = sqlite3.connect('housing.db')
-        
-        db.execute('''
-                   CREATE TABLE IF NOT EXISTS listings (
-                       address TEXT,
-                       date_seen TEXT,
-                       PRIMARY KEY (address)
-                   )
-                   ''')
-        db.close()
-        
-    def have_we_seen_this_listing(self, address):
-        db = sqlite3.connect('housing.db')
-        
-        result = db.execute(
-            'SELECT 1 FROM listings WHERE address = ?',
-            (address,)
-        ).fetchone()
-        
-        db.close()
-        
-        return result is None
+    def setup_database(self):
+        with sqlite3.connect('housing.db') as db:
+            db.execute('''
+                       CREATE TABLE IF NOT EXISTS listings(
+                           address TEXT PRIMARY KEY,
+                           date_seen TEXT
+                       )
+                       ''')
     
+    def is_listing_new(self, address):
+        with sqlite3.connect('housing.db') as db:
+            result =  db.execute(
+                'SELECT 1 FROM listings WHERE address = ?',
+                (address,)
+            ).fetchone()
+            
+            return result is None
+            
+    def save_listing(self, address):
+        with sqlite3.connect('housing.db') as db:
+            db.execute(
+                'INSERT INTO listings (address, date_seen) VALUES (?, ?)',
+                (address, datetime.now().strftime('%Y-%m-%d'))
+            )
+            
+    def clean_old_listings(self):
+        
+        with sqlite3.connect('housing.db') as db:
+            db.execute('''
+                       DELETE FROM listings
+                       WHERE date_seen < date('now', '-30 days')
+                       ''')
+            
+    def get_listings(self):
+        
+        url = ""
+        
+        querystring = {
+            "city": CITY,
+            "state_code": STATE,
+            "limit": "10",
+            "sort": "newest",
+            "price_max":str(MAX_PRICE)
+        }
+        
+        headers = {
+            "X-RapidAPI-Key": RAPIDAPI_KEY,
+            "X-RapidAPI-Host": ""
+        }
+        
+        try:
+            response = requests.get(url, headers=headers, params=querystring)
+            data = response.json()
+            
+            new_listing = []
+            for listing in data.get('listings', []):
+                address = f"{listings.get('address')} {listing.get('city')}"
+                
+                if self.is_listing_new(address):
+                    new_listings.append()
+            
